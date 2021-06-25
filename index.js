2f860f282294c2434bc04626b5cbaeb336d02435
PabloCabreraR <pablocabrera2g@gmail.com>
2021-06-26 01:40:26 +0200

    Half done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const names = require('spanish-names')
+'use strict'
 
+const uniqueRandomArray = require('unique-random-array')
+
+const maleNames = require('./spanish-male')
+const femaleNames = require('./spanish-female')
+
+exports.all = {maleNames, femaleNames}
+exports.randomMale = uniqueRandomArray(maleNames)
+exports.randomFemale = uniqueRandomArray(maleNames)
